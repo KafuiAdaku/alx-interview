a12905a29d8b21540058a74722da72761f8613ce
Dennis Adaku <dennisadaku@ymail.com>
2024-02-04 08:45:34 +0000

    added readme

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""validUTF8 module"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid utf-8 encoding
+    Args:
+        data (list): A list of integers
+
+    Returns (boolean): True or False
+    """
+
