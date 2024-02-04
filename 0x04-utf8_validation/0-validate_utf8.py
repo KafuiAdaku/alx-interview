46dcbafbf03ca3bd3dc9b1647cb1ec3fe2a25a4c
Dennis Adaku <dennisadaku@ymail.com>
2024-02-04 11:44:33 +0000

    added solution to task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,4 +10,31 @@ def validUTF8(data):
 
     Returns (boolean): True or False
     """
+    byte = 0
 
+    if not data:
+        return False
+
+    for character in data:
+        if byte == 0:
+            if ((character >> 3) & 0b111) == 0b110:
+                byte = 2
+            elif ((character >> 4) & 0b1111) == 0b1110:
+                byte = 3
+            elif ((character >> 5) & 0b11111) == 0b11110:
+                byte = 4
+            elif (character >> 7) == 0:  # and character <= 127:
+                # byte = 0
+                continue
+            else:
+                return False
+        else:
+            if (character & 0b11000000) == 0b10000000:
+                byte -= 1
+            else:
+                return False
+
+    if byte != 0:
+        return False
+
+    return True
