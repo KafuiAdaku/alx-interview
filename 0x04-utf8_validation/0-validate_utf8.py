cfa25a86fa67404b4c0f1ee1d27d4e049b646dff
Dennis Adaku <dennisadaku@ymail.com>
2024-02-04 12:52:08 +0000

    modified solution to task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,25 +16,39 @@ def validUTF8(data):
         return False
 
     for character in data:
+        bin_num = "{:08b}".format(character & 0b11111111)
         if byte == 0:
-            if ((character >> 3) & 0b111) == 0b110:
-                byte = 2
-            elif ((character >> 4) & 0b1111) == 0b1110:
-                byte = 3
-            elif ((character >> 5) & 0b11111) == 0b11110:
-                byte = 4
-            elif (character >> 7) == 0:  # and character <= 127:
-                # byte = 0
+            for bit in bin_num:
+                if bit == '0':
+                    break
+                byte += 1
+            if byte == 0:
                 continue
-            else:
+            if byte == 1 or byte > 4:
                 return False
         else:
-            if (character & 0b11000000) == 0b10000000:
-                byte -= 1
-            else:
+            if not (bin_num[0] == '1' and bin_num[1] == '0'):
                 return False
+        byte -= 1
+    return byte == 0
+    #         if ((character >> 3) & 0b111) == 0b110:
+    #             byte = 2
+    #         elif ((character >> 4) & 0b1111) == 0b1110:
+    #             byte = 3
+    #         elif ((character >> 5) & 0b11111) == 0b11110:
+    #             byte = 4
+    #         elif (character >> 7) == 0:  # and character <= 127:
+    #             # byte = 0
+    #             continue
+    #         else:
+    #             return False
+    #     else:
+    #         if (character & 0b11000000) == 0b10000000:
+    #             byte -= 1
+    #         else:
+    #             return False
 
-    if byte != 0:
-        return False
+    # if byte != 0:
+    #     return False
 
-    return True
+    # return True
